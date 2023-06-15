c0df1f19b6a7f7a080398066faff64f0d19e5ed3
Vvalenttinaa <117542074+Vvalenttinaa@users.noreply.github.com>
2023-06-15 02:01:12 +0200

    komit 1

--- MjobBackend/src/main/java/web/mjob/repositories/OglasRepository.java
+++
@@ -1,9 +0,0 @@
-package web.mjob.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import web.mjob.models.entities.OglasEntity;
-
-public interface OglasRepository extends JpaRepository<OglasEntity,Long> {
-    OglasEntity findOglasEntityById(Long id);
-
-}
